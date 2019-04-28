c86ab27b94268eb653b7187689cff49eab91fc77
danitornow <danitornow@gmail.com>
2019-04-28 12:19:49 -0500

    add log message for cloudwatch logs

--- stage1/handler.rb
+++ stage1/handler.rb
@@ -1,5 +1,6 @@
 require 'json'
 
 def hello(event:, context:)
+  p "Log message goes here!"
   { statusCode: 200, body: JSON.generate('Go Serverless v1.0! Your function executed successfully!') }
 end
