bdb1ba86760efe33b82f82e3fd48310ba1004ef4
danitornow <danitornow@gmail.com>
2019-04-28 13:42:24 -0500

    wip demo part 2

--- demo2/README.md
+++ demo2/README.md
@@ -1,3 +1,7 @@
 #Demo Part 2
 
 We're going to add some API endpoints and change the handler! GO TEAM
+
+You'll want to install the dependancies. For ruby, I used bundle. 
+
+`bundle install`
--- demo2/lib/create.rb
+++ demo2/lib/create.rb
@@ -1,14 +1,60 @@
-require 'aws-sdk'
+require 'aws-sdk-dynamodb'
 require 'json'
+require 'securerandom'
+require 'date'
 
 module Create
+  module_function
+
+  @uuid = SecureRandom.uuid
+
   def dynamo
     Aws::DynamoDB::Client.new(region: ENV['REGION'])
   end
 
-  def handler(event:, context:)
-    p "Create your dynamo entry here"
-    { statusCode: 200, body: JSON.generate('Entry created successfully') }
+  def uuid
+    @uuid
   end
 
+  def now
+    DateTime.now.strftime('%Y%m%dT%H%M%S%LZ')
+  end
+
+  def item
+    {
+      id: uuid,
+      data: ' Big New Movie',
+        info: {
+          plot: 'Nothing happens at all.',
+          timestamp: date
+        }
+    }
+  end
+
+  def data
+    begin
+      msg = message(JSON.parse(event['body']))
+    rescue StandardError => e
+      puts 'Could not parse JSON'
+    end
+
+  def params
+    {
+        table_name: ENV['DYNAMO'],
+        item: item
+    }
+  end
+
+  def handler(event:, context:)
+    puts event.inspect
+    begin
+      dynamo.put_item(params)
+      puts 'Added entry: ' + uuid.to_s
+      { statusCode: 200, body: JSON.generate('Entry created successfully') }
+    rescue  Aws::DynamoDB::Errors::ServiceError => error
+      puts 'Unable to add item: ' + uuid.to_s
+      puts error.message
+      { statusCode: 500, body: JSON.generate('It failed! :(') }
+    end
+  end
 end
